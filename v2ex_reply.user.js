f5d09d5953b2267060f49123e5f786664a057850
caoyue <i@caoyue.me>
2012-04-11 23:53:42 +0800

    change display style to v2ex

--- v2ex_reply.user.js
+++ v2ex_reply.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @id             caoyue@v2ex
 // @name           V2EX_Reply
-// @version        1.0
+// @version        1.2
 // @namespace      caoyue
 // @author         caoyue
 // @description    v2ex reply
@@ -13,8 +13,8 @@
 // ==/UserScript==
 
 // Author: caoyue (http://caoyue.me)
-// Created: 2012-04-10
-// Version: 1.1
+// Created: 2012-04-11
+// Version: 1.2
 // Updated: 2012-4-11
 
 var REPLY_TYPE = 1;  //TODO:评论显示方式. 1；Tooltip; 2：插入到评论上方；3：点击跳转到父评论
@@ -31,16 +31,18 @@ document.addEventListener('mouseover',function(e){
 			var contentArray = getContent(originID,authorName);
 			var content = "<strong>" + authorName + ":</strong><br />";
 
-			if (contentArray.length >= 2) {
+			if (contentArray.length > REPLY_COUNT) {
 				for (var i = 0; i < REPLY_COUNT; i ++ ) {
-					content = content + "<p style='padding-bottom:5px;border-bottom:1px dashed rgba(20,150,190,0.3);'>" + contentArray[contentArray.length - i - 1] + "</p>";
+					content = content + "<p style='padding-bottom:5px;border-bottom:1px dashed rgba(20,150,190,0.3);'>" + contentArray[contentArray.length - REPLY_COUNT + i] + "</p>";
 				}
 			}
-			else
-				content = content + "<p style='padding-bottom:5px;border-bottom:1px dashed rgba(20,150,190,0.3);'>" + contentArray[0] + "</p>";
+			else{
+				for (var x in contentArray){
+					content = content + "<p style='padding-bottom:5px;border-bottom:1px solid rgb(226, 226, 226);'>" + contentArray[x] + "</p>";
+				}
+			}
 
 			var layer = creatDiv(content);
-
 			layer.style.display = 'block';
 			layer.style.left = e.pageX - 60 + "px";
 			layer.style.top = e.pageY - layer.offsetHeight - 15 + "px";
@@ -91,7 +93,8 @@ function creatDiv(content){
 		layer = document.createElement("div");
 		document.body.appendChild(layer);
 		layer.setAttribute("id","ReplyToolTip");
-		layer.setAttribute("style","color:rgb(20,150,190);background-color:rgba(0,0,0,0.8);max-width:550px;max-height:500px;padding:6px 10px;position:absolute;")
+		layer.setAttribute("class","box");
+		layer.setAttribute("style","font-size:14px;background-color:rgba(255,255,255,0.9);box-shadow:0 0 10px rgba(0, 0, 0, 0.8);max-width:550px;max-height:500px;padding:6px 10px;position:absolute;")
 		layer.innerHTML = content;
 	}
 	return layer;
